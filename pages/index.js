4c5049a021d05daa2b6db0b75ba1a4eda838e790
Gabriel Valenzuela <gi.valenzuelaleon@gmail.com>
2023-05-15 23:44:55 -0400

    Up Layout, CSS and My description

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[En crecimiento]</p>
+        <p>I study Industrial Automation, I enjoy a good technical challenge. Love playing around Domotica</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
