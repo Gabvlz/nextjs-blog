672eb94cf91058095bbaa9169681d23d6ccfc822
Gabriel Valenzuela <gi.valenzuelaleon@gmail.com>
2023-05-22 21:26:46 -0400

    Created [id].js and replace code, the page content depends on external data

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I study Industrial Automation, I enjoy a good technical challenge. Love playing around Domotica</p>
+        <p>I'm Industrial Automation, I enjoy a good technical challenge. Love playing around Domotica</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
