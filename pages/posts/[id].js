89f23e8ce1acb590093ec7c5680501fc16c65bdd
Gabriel Valenzuela <gi.valenzuelaleon@gmail.com>
2023-05-23 16:59:09 -0400

    Add CSS

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,23 +2,24 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
+  return (
+    <Layout>
+      <Head>
         <title>{postData.title}</title>
-        </Head>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        <Date dateString={postData.date} />
-        <br />
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </Layout>
-    );
-  }
+      </article>
+    </Layout>
+  );
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
