12f791d5458f972f8308a4cee202a3f3cf44c392
Gabriel Valenzuela <gi.valenzuelaleon@gmail.com>
2023-05-31 12:47:58 -0400

    Changed Name

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
+const name = 'Gabriel Valenzuela León';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({
