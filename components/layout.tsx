e1ce00a24ab4e61344662936d3bd2221a3191092
Gabriel Valenzuela <gi.valenzuelaleon@gmail.com>
2023-05-31 12:02:43 -0400

    Changed Profileblog

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
+const name = 'Gabriel Valenzuela León';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({
@@ -36,7 +36,7 @@ export default function Layout({
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/profile1.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
