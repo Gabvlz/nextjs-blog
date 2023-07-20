889d468a9952f1fb1df99717b610133ccf59f3c4
Gabriel Valenzuela León <81394612+Gabvlz@users.noreply.github.com>
2023-07-20 11:59:35 -0400

    Feat/chaged info blog (#5)
    * Migrate js to typescript
    
    * Rename date.tsx
    
    * Fixed linter issues, install type packages
    
    * Changed Profileblog
    
    * Changed Name

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,10 +21,10 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Self-taught developer in apprenticeship</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I am an industrial automator, practicing javascript and typescript and enhancing my personal blog.{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
